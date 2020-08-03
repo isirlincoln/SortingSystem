64afcfa92c462ceb5a6a18e9e14f39c4eee4eccb
isirlincoln <44763135+isirlincoln@users.noreply.github.com>
2020-08-03 12:28:47 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace SortingSystem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Sort sort = new Sort();
+            int[] arrayOne = { 0, 2, 4, 6, 8, 10 };
+            int[] arrayTwo = { 1, 3, 5, 7, 9 };
+            int[] arrayThree = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 9 };
+            sort.CalculateAverage(arrayOne);
+            sort.CalculateAverage(arrayTwo);
+            sort.CalculateAverage(arrayThree);
+            Console.ReadLine();
+            sort.ReverseTheArray(arrayOne);
+            sort.ReverseTheArray(arrayTwo);
+            sort.ReverseTheArray(arrayThree);
+            Console.ReadLine();
+            Console.WriteLine("Would you like to rotate 1. Left or 2. Right?");
+            int userInputDirection = int.Parse(Console.ReadLine());
+            Console.WriteLine("How many rotations would you like?");
+            int userInputAmmount = int.Parse(Console.ReadLine());
+            Console.WriteLine("Which array would you like to rotat?\n1.Array One\n2.Array Two\n3.Array Three");
+            int userSelection = int.Parse(Console.ReadLine());
+            switch (userSelection)
+            {
+                case 1:
+                    sort.Rotate(userInputDirection, arrayOne, userInputAmmount);
+                    break;
+                case 2:
+                    sort.Rotate(userInputDirection, arrayTwo, userInputAmmount);
+                    break;
+                case 3:
+                    sort.Rotate(userInputDirection, arrayThree, userInputAmmount);
+                    break;
+                default:
+                    break;
+            }
+            sort.Bubble(arrayThree);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sort.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingSystem
+{
+    public class Sort
+    {
+        public void CalculateAverage(int[] array)
+        {
+            double mean = 0;
+            foreach (int x in array)
+            {
+                mean += x;
+            }
+            mean /= array.Length;
+            Console.WriteLine(mean);
+        }
+        public void ReverseTheArray(int[] array)
+        {
+            int z = array.Length - 1;
+            int b = 0;
+            while (b <= z)
+            {
+                int a = array[z];
+                array[z] = array[b];
+                array[b] = a;
+                z--;
+                b++;
+            }
+            Console.WriteLine(string.Join(", ", array));
+        }
+        public void Rotate(int direction, int[] array, int howManyTimes)
+        {
+            switch (direction)
+            {
+                case 1:
+                    
+                    for (int x = 0; x < howManyTimes; x++)
+                    {
+                        int n = array[0];
+                        for (int i = 0; i < array.Length - 1; i++)
+                        {
+                            array[i] = array[i + 1];
+                        }
+                        array[array.Length-1] = n;
+                    }
+                    break;
+                case 2:
+
+                    for (int x = 0; x < howManyTimes; x++)
+                    {
+                        int n = array[array.Length-1];
+                        for (int i = array.Length-1; i >=1; i--)
+                        {
+                            array[i] = array[i - 1];
+                        }
+                        array[0] = n;
+                    }
+                    break;
+            }
+            Console.WriteLine(string.Join(", ", array));
+
+        }
+        public void Bubble(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int a = 0; a < array.Length-1; a++)
+                {
+                    if (array[a] > array[a + 1])
+                    {
+                        int t = array[a + 1];
+                        array[a + 1] = array[a];
+                        array[a] = t;
+                    }
+
+                }
+            }
+            Console.WriteLine(string.Join(", ", array));
+        }
+
+    }
+}
