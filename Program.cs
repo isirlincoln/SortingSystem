a3df22560c0dc6131d2526460cb72dd51942db7f
isirlincoln <44763135+isirlincoln@users.noreply.github.com>
2020-08-03 13:40:59 -0400

    Updating

--- Program.cs
+++ Program.cs
@@ -11,19 +11,23 @@ namespace SortingSystem
             int[] arrayOne = { 0, 2, 4, 6, 8, 10 };
             int[] arrayTwo = { 1, 3, 5, 7, 9 };
             int[] arrayThree = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 9 };
+
+            //calculate average of any 1 dimensional array size.
             sort.CalculateAverage(arrayOne);
             sort.CalculateAverage(arrayTwo);
             sort.CalculateAverage(arrayThree);
             Console.ReadLine();
+            //reverse any array order
             sort.ReverseTheArray(arrayOne);
             sort.ReverseTheArray(arrayTwo);
             sort.ReverseTheArray(arrayThree);
             Console.ReadLine();
+            //rotate an array left or right
             Console.WriteLine("Would you like to rotate 1. Left or 2. Right?");
             int userInputDirection = int.Parse(Console.ReadLine());
             Console.WriteLine("How many rotations would you like?");
             int userInputAmmount = int.Parse(Console.ReadLine());
-            Console.WriteLine("Which array would you like to rotat?\n1.Array One\n2.Array Two\n3.Array Three");
+            Console.WriteLine("Which array would you like to rotate?\n1.Array One\n2.Array Two\n3.Array Three");
             int userSelection = int.Parse(Console.ReadLine());
             switch (userSelection)
             {
@@ -39,6 +43,7 @@ namespace SortingSystem
                 default:
                     break;
             }
+            //sort an array by value
             sort.Bubble(arrayThree);
             Console.ReadLine();
         }
